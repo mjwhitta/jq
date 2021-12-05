b876e643fcdbd180c1eaf65eb7f7dd3b5b379292
Miles Whittaker <mj@whitta.dev>
2021-12-05 11:44:02 -0500

    Moved generate stuff to unix only

---
+++ generate_unix.go
@@ -0,0 +1,6 @@
+//go:build !windows
+// +build !windows
+
+package jq
+
+//go:generate ./scripts/generate_go_funcs
--- globals.go
+++ globals.go
@@ -1,6 +1,4 @@
 package jq
 
-//go:generate ./scripts/generate_go_funcs
-
 // Version is the package version.
 const Version = "1.3.2"
